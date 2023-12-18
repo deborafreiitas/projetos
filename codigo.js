e4854ce8d276d4f0976099d2b074fbd53ad9cb09
deborafreiitas <153948451+deborafreiitas@users.noreply.github.com>
2023-12-18 16:06:22 -0300

    Add files via upload

---
+++ codigo.js
@@ -0,0 +1,37 @@
+// // 
+
+// // let contador = 0
+
+// // function contar() {
+// //     contador++;
+// //     document.querySelector("h1").innerHTML = contador;
+
+// //     if (contador % 10 === 0) {
+// //         alert(`contador = ${contador}`)
+// //     }
+// // }
+
+// // document.addEventListener('DOMContentLoaded'), function () {
+// //     document.querySelector('button').onclick = contar;
+// // }
+
+// document.addEventListener('DOMcontentLoadead',function(){
+// document.querySelector('form').submit=function(){
+//     const nome=document.querySelector('#nome').
+//     value;
+//     alert (`Olá,${nome}`);
+// };
+// });
+
+// document.querySelector("button").addEventListener("click", colocar)
+// let i = 0;
+// function colocar(){
+//     i++
+//     const input=document.querySelector("input").value;
+//     const h3 =document.querySelector("h3");
+//     h3.innerHTML+= `${i}. ${input}<br>`;
+// }
+
+    // JavaScript para imprimir a mensagem
+    document.getElementById("mensagem").innerText = "Olá, Mundo!";
+ 
